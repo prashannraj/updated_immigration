b8136e8e83d43536ea1534a00004060aad3efdd2
Prashann <prashannraj@hotmail.com>
2025-01-20 14:33:53 +0545

    admin okita templete set

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="footer-left">
+      <a href="techappan.com.np">Appan Technology Pvt. Ltd.</a></a>
+    </div>
+    <div class="footer-right">
+    </div>
+  </footer>
